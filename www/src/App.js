9bf5a0d29a482608035b25a08125ba3f1f4488cb
Seb <covarrubias.seb@gmail.com>
2023-06-22 21:09:31 -0700

    Created Navbar List Items for Page navigation.

--- www/src/App.js
+++ www/src/App.js
@@ -1,20 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="navbar navbar-expand">
+          <div className="container-fluid">
+          
+          <ul className="navbar-nav">
+            <li className="nav-item me-2">
+              <a href="#">Home</a>
+            </li>
+
+            <li className="nav-item me-2">
+              <a href="#">Profile</a>
+            </li>
+
+            <li className="nav-item me-2">
+              <a href="#">Notifications</a>
+            </li>
+
+            <li className="nav-item me-2">
+              <a href="#">Messages</a>
+            </li>
+
+            <li className="nav-item me-2">
+              <input type="text" placeholder="Search Birdie" />
+            </li>
+          </ul>
+
+          </div>
+        </nav>
       </header>
 
       <main>
-        <h1>Main</h1>
-      </main>
+        
+        <h1> Main Content</h1>
 
-      <footer>
-        <p>Footer</p>
-      </footer>
+      </main>
 
     </div>
   );
