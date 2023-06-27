ea75e3ffb4d4754dc44c0175e4b78da6bd600846
unknown <covarrubias.seb@gmail.com>
2023-06-26 21:11:37 -0700

    Configured server to serve completed front end build in www/build directory.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../www/build`));
 
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000;
